f7cb039be48f956c3f2f0d790056d4f0417d52ca
yaa <my@email.com>
2024-03-18 20:00:06 +0300

    Created new window

--- lll/lll/NewWindow.xaml.cs
+++ lll/lll/NewWindow.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace lll
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для NewWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewWindow : Window
     {
-        public MainWindow()
+        public NewWindow()
         {
             InitializeComponent();
         }
-
-      
     }
 }
